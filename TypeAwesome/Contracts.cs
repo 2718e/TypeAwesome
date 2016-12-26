c31575535a1ba3c8d069ce6860339e0ab9352bad
Luke <Luke@DESKTOP-CDMQKRR>
2016-12-27 12:13:26 +1300

    adding comments

--- TypeAwesome/Contracts.cs
+++ TypeAwesome/Contracts.cs
@@ -6,9 +6,21 @@ using System.Threading.Tasks;
 
 namespace TypeAwesome
 {
+
+    /// <summary>
+    /// make a model class implement this to indicate it should be exported.
+    /// </summary>
     public interface ITypedJModel { }
 
+    /// <summary>
+    /// ITypedJsonResult is used internally by the TypedJsonNetResult class to enable the constraint that returned
+    /// models must be ITypedJModel
+    /// </summary>
     public interface ITypedJsonResult { }
 
+    /// <summary>
+    /// ITypedJsonResult is used internally by the TypedJsonNetResult class to enable the constraint that returned
+    /// models must be ITypedJModel
+    /// </summary>
     public interface ITypedJsonResult<TModel> : ITypedJsonResult where TModel: ITypedJModel { }
 }
